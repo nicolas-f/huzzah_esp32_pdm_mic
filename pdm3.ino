c11841907257a56ae1fcb0ad1f2e938efdc5cbd8
nicolas-f <[omitted]>
2020-09-01 14:29:11 +0200

    reduce sample rate and precision for sending data through serial

--- pdm3.ino
+++ pdm3.ino
@@ -3,34 +3,40 @@
 
 #define READ_DELAY 3000 // millisec
 const i2s_port_t I2S_PORT = I2S_NUM_0;
-const int BLOCK_SIZE = 512;
+const int BLOCK_SIZE = 64;
 int samples[BLOCK_SIZE];
-#define SAMPLE_RATE 48000
+#define SAMPLE_RATE 24000
 long total_read = 0;
 
-// http://www.schwietering.com/jayduino/filtuino/index.php?characteristic=be&passmode=lp&order=1&usesr=usesr&sr=48000&frequencyLow=25&noteLow=&noteHigh=&pw=pw&calctype=float&run=Send
-//Low pass bessel filter order=1 alpha1=0.00052083333333333 
-class  FilterBeHp1
+// http://www.schwietering.com/jayduino/filtuino/index.php?characteristic=be&passmode=hp&order=2&usesr=usesr&sr=24000&frequencyLow=100&noteLow=&noteHigh=&pw=pw&calctype=float&run=Send
+//High pass bessel filter order=2 alpha1=0.0041666666666667 
+class  FilterBeHp2
 {
   public:
-    FilterBeHp1()
+    FilterBeHp2()
     {
       v[0]=0.0;
+      v[1]=0.0;
     }
   private:
-    float v[2];
+    float v[3];
   public:
     float step(float x) //class II 
     {
       v[0] = v[1];
-      v[1] = (9.983664252987520804e-1 * x)
-         + (0.99673285059750416082 * v[0]);
+      v[1] = v[2];
+      v[2] = (9.823849154958753660e-1 * x)
+         + (-0.96497792085018785357 * v[0])
+         + (1.96456174113331383246 * v[1]);
       return 
-         (v[1] - v[0]);
+         (v[0] + v[2])
+        - 2 * v[1];
     }
 };
 
-FilterBeHp1 highPassFilter;
+
+
+FilterBeHp2 filter;
 
 void init_pdm() {
   
@@ -43,7 +49,7 @@ void init_pdm() {
          .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT, // although the SEL config should be left, it seems to transmit on right
          .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_LSB),
          .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
-         .dma_buf_count = 4,
+         .dma_buf_count = 8,
          .dma_buf_len = BLOCK_SIZE
         };
     
@@ -93,8 +99,9 @@ void process_samples(void *pvParameters) {
             total_read += samples_read;
             float sample;
             for(int i=0; i < samples_read; i++) {
-              sample = highPassFilter.step((float)samples[i] / INT_MAX);
-              Serial.println((int)(sample * INT_MAX));
+              sample = filter.step((float)samples[i] / INT_MAX);
+              //sample = (float)samples[i] / INT_MAX;
+              Serial.println((short)(sample * SHRT_MAX));
             }
             //      float rms = 0;
             //      for(int i=0; i < nsamples; i++) {
